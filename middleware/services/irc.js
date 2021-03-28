a1b9bf30b786b754d2806824c0c2db8bff5950b2
Matias Chomicki <matias.chomicki@gmail.com>
2021-03-28 16:08:09 -0300

    Agregar soporte para más eventos en el middleware

--- middleware/services/irc.js
+++ middleware/services/irc.js
@@ -26,20 +26,77 @@ module.exports.connect = function(user, onSuccess, onError, handleEvent) {
     handleEvent('message', { from: nick, to, message: text });
   });
 
+  client.addListener('names', (channel, nicknames) =>  {
+    handleEvent('user_list', { channel, nicknames });
+  });
+
+  client.addListener('topic', (channel, topic, nickname) =>  {
+    handleEvent('topic', { channel, topic, nickname });
+  });
+
+  client.addListener('join', (channel, nickname) =>  {
+    handleEvent('join', { channel, nickname });
+  });
+
+  client.addListener('part', (channel, nickname) =>  {
+    handleEvent('part', { channel, nickname });
+  });
+
+  client.addListener('quit', (nickname, reason) =>  {
+    handleEvent('quit', { nickname, reason });
+  });
+
+  client.addListener('kick', (channel, nickname, by, reason) =>  {
+    handleEvent('kick', { channel, nickname, by, reason });
+  });
+
+  client.addListener('nick', (oldnick, newnick) =>  {
+    handleEvent('nick', { oldnick, newnick });
+  });
+
+  client.addListener('invite', (channel, from) =>  {
+    handleEvent('invited', { oldnick, newnick });
+  });
+
+  client.addListener('+mode', (channel, by, mode, argument) =>  {
+    handleEvent('+mode', { channel, by, mode, argument });
+  });
+
+  client.addListener('-mode', (channel, by, mode, argument) =>  {
+    handleEvent('-mode', { channel, by, mode, argument });
+  });
+
+  client.addListener('whois', ({ nick, user, realname, }) =>  {
+    handleEvent('whois', { nickname: nick, user, name: realname });
+  });
+
   client.addListener('error', function(message) {
     console.log('error: ', message);
-});
+    handleEvent('error', { message });
+  });
 }
 
 module.exports.say = function (userId, to, message) {
+  if (!clients[userId]) {
+    return handleEvent('disconnected', {});;
+  }
+
   clients[userId].say(to, message);
 }
 
 module.exports.join = function (userId, channel) {
+  if (!clients[userId]) {
+    return handleEvent('disconnected', {});;
+  }
+
   clients[userId].join(channel);
 }
 
 module.exports.part = function (userId, channel) {
+  if (!clients[userId]) {
+    return handleEvent('disconnected', {});;
+  }
+
   clients[userId].part(channel);
 }
 
@@ -48,5 +105,7 @@ module.exports.disconnect = function (userId) {
     clients[userId].disconnect();
 
     clients[userId] = undefined;
+
+    handleEvent('disconnected', {});
   }
 }
