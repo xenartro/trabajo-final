bb5b81534050782d353a94b82af6466156cd4e19
Matias Chomicki <matias.chomicki@gmail.com>
2021-04-18 15:13:37 -0300

    Almacenar y mostrar marca de tiempo por cada mensaje

--- client/src/components/chat/Message.jsx
+++ client/src/components/chat/Message.jsx
@@ -1,16 +1,29 @@
+import dayjs from 'dayjs';
 import ReactMarkdown from 'react-markdown';
 import User from './User';
 import 'styles/message.css';
 import { stringToMarkdown } from 'services/messages';
 
+function Timestamp(message) {
+  if (!message.ts) {
+    return null;
+  }
+
+  const ts = dayjs.unix(message.ts);
+  const now = dayjs().unix();
+
+  const timestamp = (now - ts > 86400) ? ts.fromNow() : ts.format('H:mm');
+
+  return (<small>{timestamp}</small>);
+}
+
 const Message = ({ message }) => {
-  console.log(message)
   return (
     <div className="irc__message">
       <User  avatarOnly user={message.from} />
       <div className="irc__message__bubble">
         <div className="irc__message__bubble__author">
-          {message.from}:
+          {message.from}: {Timestamp(message)}
         </div>
         <div className="irc__message__bubble__meesage">
           <ReactMarkdown>{stringToMarkdown(message.message)}</ReactMarkdown>
